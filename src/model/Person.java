2aecacc5e17e225cd8dd173b672ab19123752ba1
Pietro Guidorizzi <peterguidorizzi@gmail.com>
2026-01-19 15:08:47 -0300

    Person getter and setters with toStringMethod.

--- src/model/Person.java
+++ src/model/Person.java
@@ -10,5 +10,36 @@ public class Person {
         this.age = age;
         this.firstName = firstName;
     }
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
 }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // toString method
+
+    @Override
+    public String toString() {
+        return "Person [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + "]";
+    }
+
+}
