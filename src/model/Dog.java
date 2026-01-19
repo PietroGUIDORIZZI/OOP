a4fdbdddcc8c3290599d300e97d85586bf15cfdf
Pietro Guidorizzi <peterguidorizzi@gmail.com>
2026-01-19 15:27:22 -0300

    Created Dog Class that uses inheritance from Person

---
+++ src/model/Dog.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Dog extends Pet{
+    private String breed;
+
+    public Dog(String name, String breed) {
+        super(name);
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog [name=" + getName() + ", breed=" + breed + "]";
+    }
+}
