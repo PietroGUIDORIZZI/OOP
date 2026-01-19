9acbd7b3f6fabf65a1cf53a5a65ec0d75746c720
Pietro Guidorizzi <peterguidorizzi@gmail.com>
2026-01-19 15:06:40 -0300

    Recreated Class Person in package model and it's constructor.

---
+++ src/model/Person.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String lastName, int age, String firstName) {
+        this.lastName = lastName;
+        this.age = age;
+        this.firstName = firstName;
+    }
+}
+
